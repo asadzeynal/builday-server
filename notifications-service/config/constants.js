384fb43666709fb1c7f3fd031a0701ade3da20ff
Asad Zeynalov <asadzeynal@yahoo.com>
2018-02-27 14:11:08 +0400

    Delete constants.js

--- notifications-service/config/constants.js
+++
@@ -1,4 +0,0 @@
-exports.dbConnection = 'mongodb://dosweedos:dosweedos@tes'+
-'tcluster-shard-00-00-5tvah.mongodb.net:27017,testcluster-'+
-'shard-00-01-5tvah.mongodb.net:27017,testcluster-shard-00-02'+
-'-5tvah.mongodb.net:27017/builday-notifications-db?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin';
