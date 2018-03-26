2c3f6d5c92b0b6140e00f3f5f0c9f7824801719c
Asad Zeynalov <asadzeynal@yahoo.com>
2018-03-26 23:39:17 +0400

    Added notification capability

--- notifications-service/push/pushAdmin.js
+++ notifications-service/push/pushAdmin.js
@@ -22,21 +22,35 @@ exports.refreshFCMToken = (userEmail, token) =>
        });
    });
 
-exports.sendNotification = (message, userEmail) => {
-  var message = {
-    data: {
-      score: '850',
-      time: '2:45'
+   exports.sendNotification = (msg, userEmail) => {
+     userNotification.find({userID:userEmail})
+     .then((users) => {
+       var u = users[0];
+       msg.address = u.fcmToken;
+     }).catch(err => {
+      reject({ status: 500, message: 'Notification token not found!' });
+    });
+    var message = {
+      android: {
+      ttl: 3600 * 1000, // 1 hour in milliseconds
+      priority: 'normal',
+      notification: {
+        title: msg.title,
+        body: msg.body,
+        icon: 'stock_ticker_update',
+        color: '#f45342'
+      }
     },
-    token: 'ck9f1bmpnqg:APA91bHzKZyMa_DIeDUjV_e96DLrPK_eUXO9t2ipTtI6NaDdHXSxivNK-rIAqumc39LTV1dRcavEEd6WfNFH0PYmQ3ZIgy-9NpVMrtOmGceIwIOBkFNl85gSuwFzQ7wBexN0mWuakJGp'
-  };
-  admin.messaging().send(message)
-  .then((response) => {
-    // Response is a message ID string.
-    console.log('Successfully sent message:', response);
-  })
-  .catch((error) => {
-    console.log('Error sending message:', error);
-  });
+      token: msg.address
+    };
+    admin.messaging().send(message)
+    .then((response) => {
+      // Response is a message ID string.
+      console.log('Successfully sent message:', response);
+    })
+    .catch((error) => {
+      console.log('Error sending message:', error);
+    });
   
-}
+  }
+  
