ff6a15b90436cd623b2769aadc467719420ddea2
Asad Zeynalov <asadzeynal@yahoo.com>
2018-04-09 16:53:56 +0400

    small bug fix

--- user-service/server.js
+++ user-service/server.js
@@ -6,13 +6,10 @@ const bodyParser = require('body-parser');
 const logger        = require('morgan');
 const router        = express.Router();
 const port        = process.env.PORT || 4000;
-const rpc = require('./communication/userEventIdReceiver');
 
 app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use('/api/v1', require('./routes')());
-// var rpc = new rpc();
-rpc.receiveEventIdUser();
 
 
 app.listen(port, () => {
