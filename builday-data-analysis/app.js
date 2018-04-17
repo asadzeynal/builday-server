98842dffe0726df32f9c23966dd0933c097b10d7
Asad Zeynalov <asadzeynal@yahoo.com>
2018-04-18 01:27:25 +0400

    socket timeout bug

--- builday-data-analysis/app.js
+++ builday-data-analysis/app.js
@@ -2,6 +2,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 const request = require('request');
+process.env.UV_THREADPOOL_SIZE = 128;
 var url = 'mongodb://dosweedos:dosweedos@testcluster-shard-00-00-5tvah.mongodb.net:27017,testcluster-' +
     'shard-00-01-5tvah.mongodb.net:27017,testcluster-shard-00-02-5tvah.mongodb.net:27017/builday-events-db' +
     '?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin';
