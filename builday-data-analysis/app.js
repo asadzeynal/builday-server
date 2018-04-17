38d9889e95ee2e9d4844cfb7ae7f74330fbeff34
Asad Zeynalov <asadzeynal@yahoo.com>
2018-04-18 02:11:32 +0400

    analysis fix

--- builday-data-analysis/app.js
+++ builday-data-analysis/app.js
@@ -2,7 +2,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
 const request = require('request');
-process.env.UV_THREADPOOL_SIZE = 128;
 var url = 'mongodb://dosweedos:dosweedos@testcluster-shard-00-00-5tvah.mongodb.net:27017,testcluster-' +
     'shard-00-01-5tvah.mongodb.net:27017,testcluster-shard-00-02-5tvah.mongodb.net:27017/builday-events-db' +
     '?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin';
@@ -36,9 +35,10 @@ MongoClient.connect(url)
                 }
             })
             .then(() => {
-                events.deleteMany({});
-                process.exit(0);
+                return events.deleteMany({});
             })
+            .then((res) => {process.exit()})
+
     })
     .catch(err => {
         console.log(err)
@@ -50,6 +50,8 @@ function sendNotificationAboutNewEvent(msg,userID) {
         url: 'http://ec2-54-159-52-63.compute-1.amazonaws.com:4004/api/v1/notifications/post',
         headers: {'content-type' : 'application/json'},
         method: 'post',
+        agent: false,
+        pool: {maxSockets: 100},
         timeout: 60 * 1000,
         body: JSON.stringify({msg:msg, userID:userID})
     }, function (error, result, body) {
