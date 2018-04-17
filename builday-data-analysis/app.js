3366ead888f602e0dc120654318f1093114fd968
Asad Zeynalov <asadzeynal@yahoo.com>
2018-04-18 02:23:29 +0400

    fix

--- builday-data-analysis/app.js
+++ builday-data-analysis/app.js
@@ -1,7 +1,7 @@
 'use strict';
 const MongoClient = require('mongodb').MongoClient;
 const fs = require('fs');
-const request = require('request');
+const request = require('request-promise-native');
 var url = 'mongodb://dosweedos:dosweedos@testcluster-shard-00-00-5tvah.mongodb.net:27017,testcluster-' +
     'shard-00-01-5tvah.mongodb.net:27017,testcluster-shard-00-02-5tvah.mongodb.net:27017/builday-events-db' +
     '?ssl=true&replicaSet=TestCluster-shard-0&authSource=admin';
@@ -54,14 +54,22 @@ function sendNotificationAboutNewEvent(msg,userID) {
         pool: {maxSockets: 100},
         timeout: 60 * 1000,
         body: JSON.stringify({msg:msg, userID:userID})
-    }, function (error, result, body) {
-        if (error) {
-            console.log(error);
-        } else if (result.statusCode === 500) {
-            console.log('error');
-        } else {
-            console.log(body);
-        }
-    });
+    })
+        .then((response) => resolve({ status: 201, message: 'User Accepted Sucessfully !' }))
+
+        .catch((err) => {
+            console.log(err);
+            reject({ status: 500, message: 'Internal Server Error !' })
+        })
 }
 
+
+// , function (error, result, body) {
+//     if (error) {
+//         console.log(error);
+//     } else if (result.statusCode === 500) {
+//         console.log('error');
+//     } else {
+//         console.log(body);
+//     }
+// }
